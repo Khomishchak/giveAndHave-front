513bd49b42786ccf28ba359cafc07400df570450
Max <maksimhomisak64@gmail.com>
2023-02-09 07:44:11 +0100

    deleted jwt-service

--- src/app/authentication.guard.ts
+++ src/app/authentication.guard.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { JwtService } from './jwt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
   
-  constructor(private router: Router, private jwtService: JwtService){}
+  constructor(private router: Router){}
   
   private tokenExpired(token: string) {
     const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
